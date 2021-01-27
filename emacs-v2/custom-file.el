af12000cd29e8ea3c3c748c41619a7c963d08e96
Kevin DeMarco <kevin.demarco@gmail.com>
2021-01-26 20:11:52 -0500

    simple commit

--- emacs-v2/custom-file.el
+++ emacs-v2/custom-file.el
@@ -8,7 +8,7 @@
  '(indent-tabs-mode nil)
  '(org-agenda-files
    (quote
-    ("~/repos/private/org/inbox.org" "~/repos/private/org/gtd.org" "~/repos/private/org/schedule.org" "/home/syllogismrxs/repos/private/org/journal/2021-01-14.org")))
+    ("~/repos/private/org/inbox.org" "~/repos/private/org/gtd.org" "~/repos/private/org/schedule.org" "/home/syllogismrxs/repos/private/org/journal/2021-01-26.org")))
  '(package-archives
    (quote
     (("marmalade" . "https://marmalade-repo.org/packages/")
