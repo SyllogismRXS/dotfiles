cfa7308713feba562864f37bf5d6424c21bd6004
Kevin DeMarco <kevin@kevindemarco.com>
2021-08-06 17:58:01 -0400

    simple commit

--- emacs-v2/custom-file.el
+++ emacs-v2/custom-file.el
@@ -8,7 +8,7 @@
  '(indent-tabs-mode nil)
  '(org-agenda-files
    (quote
-    ("~/repos/private/org/inbox.org" "~/repos/private/org/gtd.org" "~/repos/private/org/schedule.org" "/home/syllogismrxs/repos/private/org/journal/2021-08-05.org")))
+    ("~/repos/private/org/inbox.org" "~/repos/private/org/gtd.org" "~/repos/private/org/schedule.org" "/home/syllogismrxs/repos/private/org/journal/2021-08-06.org")))
  '(package-archives
    (quote
     (("marmalade" . "https://marmalade-repo.org/packages/")
