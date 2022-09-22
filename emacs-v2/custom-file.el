e1fe96727e3d58970d5ac12101b5d7e92e961657
Kevin DeMarco <kevin@kevindemarco.com>
2022-09-22 17:39:41 -0400

    simple commit

--- emacs-v2/custom-file.el
+++ emacs-v2/custom-file.el
@@ -3,20 +3,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(backup-directory-alist (\` (("." \, (concat user-emacs-directory "backups")))))
+ '(backup-directory-alist `(("." \, (concat user-emacs-directory "backups"))))
  '(confirm-kill-processes nil)
  '(indent-tabs-mode nil)
  '(org-agenda-files
-   (quote
-    ("~/repos/private/org/inbox.org" "~/repos/private/org/gtd.org" "~/repos/private/org/schedule.org" "/home/syllogismrxs/repos/private/org/journal/2021-08-25.org")))
+   '("~/repos/private/org/inbox.org" "~/repos/private/org/gtd.org" "~/repos/private/org/schedule.org" "/home/syllogismrxs/repos/private/org/journal/2021-08-25.org"))
  '(package-archives
-   (quote
-    (("marmalade" . "https://marmalade-repo.org/packages/")
+   '(("marmalade" . "https://marmalade-repo.org/packages/")
      ("melpa" . "https://melpa.org/packages/")
-     ("elpa" . "https://elpa.gnu.org/packages/"))))
+     ("elpa" . "https://elpa.gnu.org/packages/")))
  '(package-selected-packages
-   (quote
-    (cmake-mode org-drill sqlite sqlite3 darkroom org-roam org-roam-bibtex docker-tramp docker-compose-mode yaml robot-mode bazel-mode pdf-tools dockerfile-mode yaml-mode helm-tramp lua-mode markdown-mode nxml-mode auctex LaTeX/P visual-fill-column helm-ag flycheck-google-cpplint google-c-style org-gcal helm-projectile projectile helm-mt multi-term org-journal deft company magit helm which-key smooth-scrolling doom-themes all-the-icons use-package)))
+   '(tj3-mode cmake-mode org-drill sqlite sqlite3 darkroom org-roam org-roam-bibtex docker-tramp docker-compose-mode yaml robot-mode bazel-mode pdf-tools dockerfile-mode yaml-mode helm-tramp lua-mode markdown-mode nxml-mode auctex LaTeX/P visual-fill-column helm-ag flycheck-google-cpplint google-c-style org-gcal helm-projectile projectile helm-mt multi-term org-journal deft company magit helm which-key smooth-scrolling doom-themes all-the-icons use-package))
  '(use-package-always-defer t)
  '(use-package-always-ensure nil)
  '(use-package-verbose nil))
