b3b6f168f3ae392e56402c895e43308f9ab3718e
Kevin DeMarco <kevin@kevindemarco.com>
2024-06-05 11:33:01 -0400

    Lower resolution on awm6r2

--- awesome/autorun_awm16r2.sh
+++ awesome/autorun_awm16r2.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Enable tap-to-click on trackpad
 xinput set-prop "$(xinput list --name-only | grep -i touch)" "libinput Tapping Enabled" 1
+
+# Lower resolution, default is 2560x1600
+xrandr -s 1920x1200
 
 touch ~/.running_awm16r2.txt
