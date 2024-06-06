228c1de55623c42f06730afb40aa8c3f8dc8a427
Kevin DeMarco <kevin@kevindemarco.com>
2024-06-06 18:55:09 -0400

    simple commit

--- awesome/autorun_awm16r2.sh
+++ awesome/autorun_awm16r2.sh
@@ -4,6 +4,6 @@
 xinput set-prop "$(xinput list --name-only | grep -i touch)" "libinput Tapping Enabled" 1
 
 # Lower resolution, default is 2560x1600
-xrandr -s 1920x1200
+#xrandr -s 1920x1200
 
 touch ~/.running_awm16r2.txt
