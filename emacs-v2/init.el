767bb7604210b9394921ec4cf5e3250a9e80ee4e
Kevin DeMarco <kevin@kevindemarco.com>
2026-01-07 08:10:45 -0500

    simple commit

--- emacs-v2/init.el
+++ emacs-v2/init.el
@@ -428,19 +428,20 @@
   ;(add-hook 'org-capture-after-finalize-hook (lambda () (org-gcal-sync) ))
   )
 
-(use-package aidermacs
-  :bind (("C-c a" . aidermacs-transient-menu))
-  :config
-  ; Set API_KEY in .bashrc, that will automatically picked up by aider or in elisp
-  ;(setenv "ANTHROPIC_API_KEY" "sk-...")
-  ; defun my-get-openrouter-api-key yourself elsewhere for security reasons
-  ;(setenv "OPENROUTER_API_KEY" (my-get-openrouter-api-key))
-  :custom
-  ; See the Configuration section below
-  (aidermacs-default-chat-mode 'architect)
-  ;(aidermacs-default-model "openrouter/google/gemini-2.0-flash-exp:free")
-  (aidermacs-default-model "gemini/gemini-3-pro-preview")
-  )
+(if (string= (system-name) "m16R2")
+    (use-package aidermacs
+      :bind (("C-c a" . aidermacs-transient-menu))
+      :config
+       ; Set API_KEY in .bashrc, that will automatically picked up by aider or in elisp
+       ;(setenv "ANTHROPIC_API_KEY" "sk-...")
+       ; defun my-get-openrouter-api-key yourself elsewhere for security reasons
+       ;(setenv "OPENROUTER_API_KEY" (my-get-openrouter-api-key))
+      :custom
+      ; See the Configuration section below
+      (aidermacs-default-chat-mode 'architect)
+      ;(aidermacs-default-model "openrouter/google/gemini-2.0-flash-exp:free")
+      (aidermacs-default-model "gemini/gemini-3-pro-preview")
+      ))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Setup Google C / C++ Style
